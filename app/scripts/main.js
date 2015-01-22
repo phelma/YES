ecffe60c0801c9fe48c23c69d956f3862509a41c
Phil Helm <philip.helm@omgplc.com>
2015-01-22 18:14:24 +0000

    init

---
+++ app/scripts/main.js
@@ -0,0 +1,37 @@
+/* jshint devel:true */
+'use strict';
+// var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dc6zaTOxFJmzC&limit=5");
+// xhr.done(function(data) {
+// 	console.log("success got data", data);
+// });
+
+$('#bigtext').fitText(0.25);
+
+var interval;
+var gifUrl;
+
+function slideshow() {
+	$.support.cors = true;
+	$.getJSON("http://api.giphy.com/v1/gifs/tv?api_key=dc6zaTOxFJmzC&tag=giphytrending", null, function(response) {
+		var id = response.data.id;
+		var image = response.data.image_original_url;
+
+		$('#gif').css('background-image', 'url(' + image + ')');
+
+		gifUrl = 'http://giphy.com/gifs/' + id;
+
+	}).error(function(jqXHR, textStatus, e) {
+		console.log(e);
+		next();
+	});
+};
+
+function next() {
+	clearInterval(interval);
+	slideshow();
+	interval = setInterval(slideshow, 10000);
+}
+interval = setInterval(slideshow, 10000);
+
+slideshow();
+
